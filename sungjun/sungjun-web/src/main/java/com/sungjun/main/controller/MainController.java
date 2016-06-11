8cd0f4414cdcaa3fbaa503b6f8a0568e5c4260ea
SungJun <arduous1130@gmail.com>
2016-06-11 17:23:34 +0900

    [2016-06-11] Push main / news pages.

--- sungjun/sungjun-web/src/main/java/com/sungjun/main/controller/MainController.java
+++ sungjun/sungjun-web/src/main/java/com/sungjun/main/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.sungjun.main.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
@@ -24,7 +26,15 @@ public class MainController {
 		Map<String, String> sidebarMenuData = new HashMap<String, String>();
 		sidebarMenuData.put("home", "active");
 		
+		RestTemplate restTemplate = new RestTemplate();
+		Map<String, Object> dailyQTData		= restTemplate.getForObject("http://localhost:8081/api/news/daily-qt", Map.class);
+		List<Object> naverRealRankList		= restTemplate.getForObject("http://localhost:8081/api/news/naver-realrank", List.class);
+		List<Object> daumRealRankList		= restTemplate.getForObject("http://localhost:8081/api/news/daum-realrank", List.class);
+		
 		model.addAttribute("sidebarMenuData", sidebarMenuData);
+		model.addAttribute("dailyQTData", dailyQTData);
+		model.addAttribute("naverRealRankList", naverRealRankList);
+		model.addAttribute("daumRealRankList", daumRealRankList);
 		
 		return "/main/main";
 	}
